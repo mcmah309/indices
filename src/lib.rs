46d7afc5db79d6aab355f8f169aeb0f230f60e02
mcmah309 <dillonmcmahon79@gmail.com>
2024-04-21 18:10:23 +0000

    feat: Add checks to two

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,22 @@
 
 
-pub fn two<T>(slice: &mut [T], in_index1: usize, in_index2: usize) -> (&mut T, &mut T) {
-    let mut in_indices = [in_index1, in_index2];
-    let mut out_indices = [0, 1];
-    // in_indices.sort();
-    // let args_len = in_indices.len();
-    // let slice_len = slice.len();
-    // for i in 0..args_len {
-    //     if in_indices[i] >= slice_len {
-    //         panic!("")
-    //     }
-    //     if i != args_len - 1 && in_indices[i] == in_indices[i + 1] {
-    //         panic!("")
-    //     }
-    // }
+pub fn two<T>(slice: &mut [T], index0: usize, index1: usize) -> (&mut T, &mut T) {
+    let mut indices = [index0, index1];
+    indices.sort();
+    let args_len = indices.len();
+    let slice_len = slice.len();
+    for i in 0..args_len {
+        if indices[i] > slice_len - 1 {
+            panic!("Index out of bounds. Requested index was `{}` while slice length was `{}`.", indices[i], slice_len)
+        }
+        if i != args_len - 1 && indices[i] == indices[i + 1] { // todo replace with unchecked
+            panic!("Duplicate indices are not allowed. Index `{}` was requested twice.", indices[i])
+        }
+    }
     let ptr = slice.as_mut_ptr();
     unsafe {
-        let out0 = &mut *ptr.add(in_indices[out_indices[0]]);
-        let out1 = &mut *ptr.add(in_indices[out_indices[1]]);
+        let out0 = &mut *ptr.add(index0);
+        let out1 = &mut *ptr.add(index1);
         return (out0, out1);
     }
 }
@@ -30,7 +29,19 @@ mod tests {
     fn it_works() {
         let mut data = [5,4,3,2,1];
         let slice = data.as_mut_slice();
-        let result = two(slice, 1, 3);
-        assert_eq!(result, (&mut 4,&mut 2));
+        let (one, two) = two(slice, 1, 3);
+        assert_eq!(one, &mut 4);
+        assert_eq!(two, &mut 2);
+        *one = 10;
+        *two = 20;
+        assert_eq!(data, [5,10,3,20,1]);
     }
+
+    // #[test]
+    // fn it_works2() {
+    //     let mut data = [5,4,3,2,1];
+    //     let slice = data.as_mut_slice();
+    //     let (one, two) = two(slice, 3, 3);
+    //     println!("{}",one)
+    // }
 }
