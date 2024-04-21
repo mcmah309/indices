7e7843328745eb123df542ffa863c640fe7330d7
mcmah309 <dillonmcmahon79@gmail.com>
2024-04-21 19:08:37 +0000

    feat: Add indices_ordered

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,46 @@ macro_rules! indices {
         let mut indices = [$($index),*];
         indices.sort();
 
-        let args_len = indices.len();
-        let slice_len = slice.len();
-        for i in 0..args_len {
-            if indices[i] > slice_len - 1 {
-                panic!("Index out of bounds. Requested index was `{}` while slice length was `{}`.", indices[i], slice_len)
+        let indices_len_minus_one = indices.len() - 1;
+        let slice_len_minus_one = slice.len() - 1;
+        for i in 0..indices_len_minus_one {
+            if indices[i] > slice_len_minus_one {
+                panic!("Index out of bounds. Requested index was `{}` while slice length was `{}`.", indices[i], slice_len_minus_one + 1)
             }
-            if i != args_len - 1 && indices[i] == indices[i + 1] { // todo replace with unchecked
+            if indices[i] == indices[i + 1] {
                 panic!("Duplicate indices are not allowed. Index `{}` was requested twice.", indices[i])
             }
+        }
+        if indices[indices_len_minus_one] > slice_len_minus_one {
+            panic!("Index out of bounds. Requested index was `{}` while slice length was `{}`.", indices[indices_len_minus_one], slice_len_minus_one + 1)
+        }
+
+        let ptr = slice.as_mut_ptr();
+        (
+            $(unsafe { &mut *ptr.add($index) },)*
+        )
+    }};
+}
+
+/// A macro that returns mutable references for the requested indices, assumes the requested indices are already ordered
+/// smallest to largest. Panics if any index is out of bounds, if any index is duplicated, or if not sorted.
+macro_rules! indices_ordered {
+    ($slice:expr, $( $index:expr ),*) => {{
+        let slice = $slice;
+        let indices = [$($index),*];
+
+        let indices_len_minus_one = indices.len() - 1;
+        let slice_len_minus_one = slice.len() - 1;
+        for i in 0..indices_len_minus_one {
+            if indices[i] > slice_len_minus_one {
+                panic!("Index out of bounds. Requested index was `{}` while slice length was `{}`.", indices[i], slice_len_minus_one + 1)
+            }
+            if indices[i] >= indices[i + 1] {
+                panic!("Indices not sorted or duplicate indices detected.")
+            }
+        }
+        if indices[indices_len_minus_one] > slice_len_minus_one {
+            panic!("Index out of bounds. Requested index was `{}` while slice length was `{}`.", indices[indices_len_minus_one], slice_len_minus_one + 1)
         }
 
         let ptr = slice.as_mut_ptr();
@@ -30,7 +61,7 @@ macro_rules! indices {
 mod tests {
     
     #[test]
-    fn it_works() {
+    fn indices_works() {
         let mut data = [5,4,3,2,1];
         let slice = data.as_mut_slice();
         let (one, two) = indices!(slice, 1, 3);
@@ -42,7 +73,7 @@ mod tests {
     }
 
     #[test]
-    fn out_of_order() {
+    fn indices_out_of_order() {
         let mut data = [5,4,3,2,1];
         let slice = data.as_mut_slice();
         let (one, two) = indices!(slice, 3, 1);
@@ -54,7 +85,7 @@ mod tests {
     }
 
     #[test]
-    fn more_than_two_indices() {
+    fn indices_more_than_two_indices() {
         let mut data = [5,4,3,2,1];
         let slice = data.as_mut_slice();
         let (one, two, three) = indices!(slice, 3, 1, 2);
@@ -65,5 +96,45 @@ mod tests {
         *two = 20;
         *three = 30;
         assert_eq!(data, [5,20,30,10,1]);
+    }
+
+    //************************************************************************//
+
+    #[test]
+    fn indices_ordered_works() {
+        let mut data = [5,4,3,2,1];
+        let slice = data.as_mut_slice();
+        let (one, two) = indices_ordered!(slice, 1, 3);
+        assert_eq!(one, &mut 4);
+        assert_eq!(two, &mut 2);
+        *one = 10;
+        *two = 20;
+        assert_eq!(data, [5,10,3,20,1]);
+    }
+
+    // #[test]
+    // fn indices_ordered_out_of_order() {
+    //     let mut data = [5,4,3,2,1];
+    //     let slice = data.as_mut_slice();
+    //     let (one, two) = indices_ordered!(slice, 3, 1);
+    //     assert_eq!(one, &mut 2);
+    //     assert_eq!(two, &mut 4);
+    //     *one = 10;
+    //     *two = 20;
+    //     assert_eq!(data, [5,20,3,10,1]);
+    // }
+
+    #[test]
+    fn indices_ordered_more_than_two_indices() {
+        let mut data = [5,4,3,2,1];
+        let slice = data.as_mut_slice();
+        let (one, two, three) = indices_ordered!(slice, 1, 2, 4);
+        assert_eq!(one, &mut 4);
+        assert_eq!(two, &mut 3);
+        assert_eq!(three, &mut 1);
+        *one = 10;
+        *two = 20;
+        *three = 30;
+        assert_eq!(data, [5,10,20,2,30]);
     }
 }
