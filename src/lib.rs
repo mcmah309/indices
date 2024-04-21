d8c174ec15d06a57d9257a4a425bb0fc820b4ed7
mcmah309 <dillonmcmahon79@gmail.com>
2024-04-21 17:47:11 +0000

    feat: Initial split 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+
+pub fn two<T>(slice: &mut [T], in_index1: usize, in_index2: usize) -> (&mut T, &mut T) {
+    let mut in_indices = [in_index1, in_index2];
+    let mut out_indices = [0, 1];
+    // in_indices.sort();
+    // let args_len = in_indices.len();
+    // let slice_len = slice.len();
+    // for i in 0..args_len {
+    //     if in_indices[i] >= slice_len {
+    //         panic!("")
+    //     }
+    //     if i != args_len - 1 && in_indices[i] == in_indices[i + 1] {
+    //         panic!("")
+    //     }
+    // }
+    let ptr = slice.as_mut_ptr();
+    unsafe {
+        let out0 = &mut *ptr.add(in_indices[out_indices[0]]);
+        let out1 = &mut *ptr.add(in_indices[out_indices[1]]);
+        return (out0, out1);
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +28,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut data = [5,4,3,2,1];
+        let slice = data.as_mut_slice();
+        let result = two(slice, 1, 3);
+        assert_eq!(result, (&mut 4,&mut 2));
     }
 }
